093156f3ddcbf3cecf19a9bd2ecaf26fe436930e
Mahmood <mahmood_1526@yahoo.com>
2020-11-21 13:25:51 +0330

    Auth components and context

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,31 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 }
 
